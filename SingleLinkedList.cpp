cdb51613b70971ca1e390a7456b37efbdc035a32
iizaghz <sukmahawaiza@gmail.com>
2025-05-08 17:04:25 +0700

    add fungsi dellNode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -79,4 +79,20 @@ class List
         return (*current != NULL);
         }
 
-   
+    bool delNode (int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START =START ->next;
+        else    
+             previous->next = current->next;
+        delete current;
+        return true;
+    }
+
+
+
+ 
