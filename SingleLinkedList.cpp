748091fd9a15534944b36c75cb12208abb6d55bf
iizaghz <sukmahawaiza@gmail.com>
2025-05-08 17:13:55 +0700

    add case 4

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -161,7 +161,30 @@ int main()
             mhs.traverse();
         }
         break;
-        
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                << "Masukkan no mahasiswa yang dicari :";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                    << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                    << "Data ditemukan << endl";
+                    cout << "\nNo Mahasiswa :"<< current->noMhs << endl;
+                    cout << "\n";
+            }
+        }
+
+    
     }
 }
 }
