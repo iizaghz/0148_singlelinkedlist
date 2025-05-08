de3f88b44f4ffbee3b4b187074ab7252bb3836b3
iizaghz <sukmahawaiza@gmail.com>
2025-05-08 17:10:23 +0700

    add output menu

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -120,4 +120,20 @@ int main()
     List mhs;
     int nim;
     char ch;
+    while (1)
+    {
+        cout
+            << "Menu" << endl;
+        cout << endl
+            << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data didalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout
+            << "Masukkan pilihan (1-5):" << endl;
+        cin >> ch;
+        switch (ch)
+        {
+        
 }
